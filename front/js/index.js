317e92362f98d61107f7cc9ca504bd499fa4db31
MaevaPrecourt <maeva.precourt@gmail.com>
2023-01-18 17:00:57 +0100

    Modifs index - 18/01

--- front/js/index.js
+++ front/js/index.js
@@ -1,1 +1,17 @@
-fetch("http://localhost:3000/api/products");
+//api_url
+const apiProducts="http://localhost:3000/api/products";
+
+//defining_async_function
+async function getApi(url) {
+
+//storing_response
+const response = await fetch(url);}
+
+//calling_that_async_function
+getApi(apiProducts);
+
+
+/** Ressources Utilisées - Mentorat
+ * https://openclassrooms.com/fr/courses/7697016-creez-des-pages-web-dynamiques-avec-javascript/7911151-envoyez-une-requete-depuis-le-navigateur
+ * https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/
+*/

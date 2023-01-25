ba8b844c77f1f3a3dd660ff4151a6944b8bead7d
MaevaPrecourt <maeva.precourt@gmail.com>
2023-01-25 17:10:09 +0100

    Page D'Accueil - 25/01

--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,45 @@
-//api_url
-const apiProducts="http://localhost:3000/api/products";
+//Requête de l'API et demande de l'ensemble des produits
+fetch("http://localhost:3000/api/products")
 
-//defining_async_function
-async function getApi(url) {
+//Récupération et conversion de la réponse émise au format JSON
+.then(response => response.json())
 
-//storing_response
-const response = await fetch(url);}
+//Parcours du résultat
+.then(couches => {
+  
+  /**Section au contenu commenté dans "index.html"
+  *Contenu requêté via la classe ".items"*/
+  const catalog = document.querySelector(".items")
 
-//calling_that_async_function
-getApi(apiProducts);
+  /**Tableau d'objets (cf. "Product.js")
+  *Si tableau, alors boucle "for ... of ..."*/
+  for (let couch of couches){
+  
+    //anchorElement = Product Link
+    const anchorElement = document.createElement("a")
+    catalog.appendChild(anchorElement)
+    anchorElement.href = "./product.html?id=" + couch._id
 
+    //cardElement = Product Card
+    const cardElement = document.createElement("article")
+    anchorElement.appendChild(cardElement)
 
-/** Ressources Utilisées - Mentorat
- * https://openclassrooms.com/fr/courses/7697016-creez-des-pages-web-dynamiques-avec-javascript/7911151-envoyez-une-requete-depuis-le-navigateur
- * https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/
-*/
+    //imageElement = Product Image
+    const imageElement = document.createElement("img")
+    cardElement.appendChild(imageElement)
+    imageElement.src = couch.imageUrl
+    imageElement.alt = couch.altTxt
+
+    //headingElement = Product Name
+    const headingElement = document.createElement("h3")
+    headingElement.classList.add("productName")
+    cardElement.appendChild(headingElement)
+    headingElement.innerHTML = couch.name
+
+    //paragraphElement = Product Description
+    const paragraphElement = document.createElement("p")
+    paragraphElement.classList.add("productDescription")
+    cardElement.appendChild(paragraphElement)
+    paragraphElement.innerHTML = couch.description
+  }
+})
